8377f84065cea38e7ffbf2eca9f8d66e7f2fda5a
Rahul R. Kadam <rkadam01@dc2.propsol.org>
2020-05-18 12:45:56 +0530

    Updates

---
+++ precache-manifest.96cfa2fc18c0e5eb131a17a6a1747520.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0d7ea6b1a962f109b648b402adf786",
+    "url": "/rkkadam818/cart/index.html"
+  },
+  {
+    "revision": "17b95ddf2c3fb6d86e71",
+    "url": "/rkkadam818/cart/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1c387edcaf479fc2f8b2",
+    "url": "/rkkadam818/cart/static/css/main.0f41ecb2.chunk.css"
+  },
+  {
+    "revision": "17b95ddf2c3fb6d86e71",
+    "url": "/rkkadam818/cart/static/js/2.4b5c9464.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rkkadam818/cart/static/js/2.4b5c9464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c387edcaf479fc2f8b2",
+    "url": "/rkkadam818/cart/static/js/main.0ccfb6f3.chunk.js"
+  },
+  {
+    "revision": "7bcb7fd4e7ed21382837",
+    "url": "/rkkadam818/cart/static/js/runtime-main.835bb548.js"
+  },
+  {
+    "revision": "12da7c4ff122f046485b30b1358d8118",
+    "url": "/rkkadam818/cart/static/media/product.12da7c4f.jpg"
+  }
+]);
